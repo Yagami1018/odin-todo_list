709fff894b9b6c79cbb41c27140a6b71b7e63368
XNAK3 <yagami01018@gmail.com>
2026-07-01 15:07:24 -0400

    Implement functions to add tasks

--- src/index.js
+++ src/index.js
@@ -3,12 +3,39 @@ import {} from "./style.css";
 //DOM references
 const taskBtn = document.querySelector(".form-btn");
 const task = document.querySelector(".form-input");
+const taskList = document.querySelector(".tasks-list");
 
 //Functions
+function addTask(name) {
+    const listItem = document.createElement("li");
+    const taskName = document.createElement("p");
+    const buttons = document.createElement("div");
+    const toggleBtn = document.createElement("button");
+    const removeBtn = document.createElement("button");
+
+    //Styles
+    buttons.classList.add('task-btns')
+    listItem.classList.add("task-item");
+    taskName.classList.add("task-name");
+    toggleBtn.classList.add("toggle-btn");
+    removeBtn.classList.add("rm-btn");
+
+    //Text Contents
+    toggleBtn.textContent = "Toggle";
+    removeBtn.textContent = "Remove";
+    taskName.textContent = name;
+
+    buttons.append(toggleBtn, removeBtn);
+    listItem.append(taskName, buttons);
+    taskList.appendChild(listItem);
+}
+
+//Handlers
 const handleAddBtn = (event) => {
     event.preventDefault();
-    let text = task.value;
-    console.log(`Hello stupid ${text}`);
+    if (task.value)
+        addTask(task.value);
+    task.value = ""
 };
 
 //Events
