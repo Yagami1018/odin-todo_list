9556d1b4311d0c359e949f25bb4b1e5b43446bdb
XNAK3 <yagami01018@gmail.com>
2026-07-01 14:27:27 -0400

    Implement basic layout to form for adding tasks and a sample on the list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import { } from "./style.css"
+import {} from "./style.css";
+
+//DOM references
+const taskBtn = document.querySelector(".form-btn");
+const task = document.querySelector(".form-input");
+
+//Functions
+const handleAddBtn = (event) => {
+    event.preventDefault();
+    let text = task.value;
+    console.log(`Hello stupid ${text}`);
+};
+
+//Events
+taskBtn.addEventListener("click", (e) => handleAddBtn(e));
